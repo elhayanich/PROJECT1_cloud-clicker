eeb4ea94756ab9f9605bb8eebfa3910332dea52d
Aurélien Lavanchy <lavanchy.aurelien@gmail.com>
2024-03-19 15:36:23 +0100

    feat: Added favicon.ico, implemented the  &  functions

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ let enteredName = "Anonymous";
 //? les bonus (à définir)
 const bonusOne = document.querySelector("#bonus-1");
 let isBonusOneActive = false;
+let isBonusOneUnlocked = false;
+let bonusOneCost = 100;
 let bonusOneNumber = 0;
 const bonusTwo = document.querySelector("#bonus-2");
 let isBonusTwoActive = false;
+let isBonusTwoUnlocked = false;
+let bonusTwoCost = 750;
 let bonusTwoNumber = 0;
 const bonusThree = document.querySelector("#bonus-3");
 let isBonusThreeActive = false;
+let isBonusThreeUnlocked = false;
+let bonusThreeCost = 2500;
 let bonusThreeNumber = 0;
 //? les paliers (à définir)
 const stepOne = 100;
@@ -30,42 +36,51 @@ const menuList = document.querySelector(".menu-list");
 //* Fonctions, événements & affectations
 // fonction qui va s'occuper de changer l'arrière-plan
 function checkStep() {
-    if (score >= stepTwo && score < stepThree) {
-        backgroundImage.style.backgroundImage =
-            "url(./assets/background-2.png)";
-        scoreMultiplier = 2;
-    }
-    if (score >= stepThree && score < stepFour) {
-        backgroundImage.style.backgroundImage =
-            "url(./assets/background-3.png)";
-        scoreMultiplier = 4;
-    }
-    if (score >= stepFour) {
-        backgroundImage.style.backgroundImage =
-            "url(./assets/background-4.png)";
-        scoreMultiplier = 8;
-    }
+  if (score >= stepTwo && score < stepThree) {
+    backgroundImage.style.backgroundImage = "url(./assets/background-2.png)";
+    scoreMultiplier = 2;
+    isBonusOneUnlocked = true;
+  }
+  if (score >= stepThree && score < stepFour) {
+    backgroundImage.style.backgroundImage = "url(./assets/background-3.png)";
+    scoreMultiplier = 4;
+    isBonusTwoUnlocked = true;
+  }
+  if (score >= stepFour) {
+    backgroundImage.style.backgroundImage = "url(./assets/background-4.png)";
+    scoreMultiplier = 8;
+    isBonusThreeUnlocked = true;
+  }
+}
+function updateScore() {
+  scoreDisplay.innerHTML = score;
+  checkStep();
 }
 // le nom du joueur une fois entré sera stocké ici
 playerName.innerHTML = enteredName;
 // fonction qui gère l'augmentation du score
-function augmentScore(a) {
-    score = score + a * scoreMultiplier;
-    scoreDisplay.innerHTML = score;
+function augmentScore(a = bonusOneNumber) {
+  score = score + a * scoreMultiplier;
+  updateScore();
 }
 // événement qui augmente le score à chaque fois qu'on clicke sur le nuage
 cloud.addEventListener("click", () => {
-    augmentScore(1);
+  augmentScore(1);
+  // ajouter une animation (CSS?) quand on clicke sur le nuage
 });
 // fonctions pour les bonus
 bonusOne.addEventListener("click", () => {
+  if (score >= bonusOneCost) {
+    score -= bonusOneCost;
+    updateScore();
     bonusOneNumber++;
     if (!isBonusOneActive) {
-        isBonusOneActive = true;
-        setInterval(augmentScore(2) * bonusOneNumber, 1500);
+      isBonusOneActive = true;
+      setInterval(augmentScore, 1500);
     }
+  }
 });
 // fonction qui gère le menu burger
 burgerMenu.addEventListener("click", function () {
-    menuList.classList.toggle("active");
+  menuList.classList.toggle("active");
 });
